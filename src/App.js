e36b517cee7f93b5c53c86bae3b6ba2ee596de9a
issaquahsun <xyapple28@gmail.com>
2020-02-23 20:28:27 -0800

    add search bar state and props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { CardList } from './components/card-list/card-list.component';
 import './App.css';
+import{SearchBox} from './components/search-box/search-box.component';
 
 class App extends Component {
 	constructor() {
 		super();
 		this.state = {
-			monsters: []
+			monsters: [],
+			searchField: ''
 		};
 	}
 	componentDidMount() {
@@ -16,9 +18,14 @@ class App extends Component {
 	}
 
 	render() {
+    const {monsters, searchField}=this.state;
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()));
 		return (
 			<div className='App'>
-				<CardList monsters={this.state.monsters}></CardList>
+        <SearchBox 
+        placeholder='search monsters' 
+        handleChange={e=>this.setState({searchField: e.target.value})}/>
+				<CardList monsters={filteredMonsters}></CardList>
 			</div>
 		);
 	}
