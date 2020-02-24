5876a013fa9c1073ac391751dde9ddfd4da8a398
issaquahsun <xyapple28@gmail.com>
2020-02-23 16:41:53 -0800

    fetch the API and set monsters names

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
 	constructor() {
 		super();
 		this.state = {
-			string: 'Hello'
+			monsters: []
 		};
 	}
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then((response) => response.json())
+			.then((users) => this.setState({ monsters: users }));
+	}
+
 	render() {
-		return (
-			<div className="App">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<p>{this.state.string}</p>
-					<button onClick={() => this.setState({ string: 'Hello Friends!' })}>Change Text</button>
-					<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-						Learn React
-					</a>
-				</header>
-			</div>
-		);
+		return <div className="App">{this.state.monsters.map((monster) => <h1 key={monster.id}>{monster.name}</h1>)}</div>;
 	}
 }
 
