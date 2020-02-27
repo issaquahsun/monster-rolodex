bb39a14469ef2248799785a17d62e29ec6519c2f
issaquahsun <xyapple28@gmail.com>
2020-02-26 20:28:12 -0800

    Updates

---
+++ precache-manifest.26d6b4e0155406a5b2a8376798ddfa8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39802f6cc0b1d859bf15cd93b53795f3",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "225e9804580f2efc6bb4",
+    "url": "/monster-rolodex/static/css/main.948db806.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monster-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monster-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "225e9804580f2efc6bb4",
+    "url": "/monster-rolodex/static/js/main.1a3bf084.chunk.js"
+  },
+  {
+    "revision": "bc09d86fba44e1c7ba83",
+    "url": "/monster-rolodex/static/js/runtime-main.8000f3b5.js"
+  }
+]);
